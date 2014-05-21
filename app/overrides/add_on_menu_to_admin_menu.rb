778e85d638440b197cbfae649a568499ebbbe1e4
digitalm <mika.mizuno@gmail.com>
2014-05-21 17:27:02 +0900

    modify add_on_menu

--- app/overrides/add_on_menu_to_admin_menu.rb
+++ app/overrides/add_on_menu_to_admin_menu.rb
@@ -2,7 +2,5 @@ Deface::Override.new(:virtual_path => 'spree/admin/shared/_menu',
   :name => 'add_on_menu_to_admin_menu',
   :insert_bottom => "[data-hook='admin_tabs']",
   :text => "
-<% if can? :admin, Spree::AddOnMaker %>
-<%= tab(:AddOnMenus, :url => spree.admin_add_on_menu_path, :icon => 'icon-th-large') %>
-<% end %>
+<%= tab(:add_on_menu, :label => 'add_on_menu', :url => spree.admin_add_on_menu_path, :icon => 'icon-th-large') %>
 ")
